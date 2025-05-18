66fada0ff58997fb0f06fabfb9ff9a3d869ddc8d
Brian Ferri <me@brianferri.dev>
2025-05-18 15:52:49 +0200

    chore(zuws): formatting

--- zig/zuws/main.zig
+++ zig/zuws/main.zig
@@ -3,16 +3,16 @@ const App = zuws.App;
 const Request = zuws.Request;
 const Response = zuws.Response;
 
+fn handle_client(res: *Response, req: *Request) void {
+    _ = req;
+    res.end("Hello World!\n", false);
+}
+
 pub fn main() !void {
     const app: App = try .init();
     defer app.deinit();
 
-    _ = app.get("/*", struct {
-        fn f(res: *Response, req: *Request) void {
-            _ = req;
-            res.end("Hello World!\n", false);
-        }
-    }.f);
-
-    try app.listen(3000, null);
+    try app
+        .get("/*", handle_client)
+        .listen(3000, null);
 }
